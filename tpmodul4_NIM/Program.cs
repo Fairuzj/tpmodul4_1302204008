966aeafd529d43179023e88890c42a419fda33b6
Fairuzj <fairuzjmf@gmail.com>
2022-03-18 16:55:59 +0700

    + Menambahkan class DataGeneric + mengganti nama projek //lupa nulis nim // lupa commit

--- tpmodul4_NIM/Program.cs
+++ tpmodul4_NIM/Program.cs
@@ -10,6 +10,33 @@ namespace tpmodul4_NIM
     {
         static void Main(string[] args)
         {
+            Console.Write("Nama User : ");
+
+            HaloGeneric.SapaUser<String>(Console.ReadLine());
+            Console.ReadLine();
+
+            DataGeneric<String>(Console.ReadLine()).PrintData();
         }
     }
+    class HaloGeneric
+    {
+        public static void SapaUser<X> (X x)
+        {
+            Console.WriteLine("Halo user " + x);
+        }
+    }
+    class DataGeneric<T>
+    {
+        T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah : " + data);
+        }
+    }
+
 }
